3f168161b103de2bb21c91f707f790fde2eeece4
David Votrubec <david.votrubec@gmail.com>
2016-02-03 14:43:09 +0100

    Added short bio

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@
 //import typing = require('./simulateTyping');
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    var text = "Hi, my name's David and I am a web developer";
+    var text = "Hi, I am a web developer";
     
     // simulateTyping() returns a Promise
     simulateTyping(text, 'greetings').then(() => {
         
-        var element = document.getElementById('skillset');
+        var skillSet = document.getElementById('skillset'),
+            shortBio = document.getElementById('short-bio');
         
         // TODO: Perform some CSS animations
-        removeClass(element, 'visuallyhidden');        
+        removeClass(skillSet, 'visuallyhidden');
+        removeClass(shortBio, 'visuallyhidden');        
     });
     
 });
