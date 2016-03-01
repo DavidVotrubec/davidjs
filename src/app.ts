b11e3cc3414683101f3ef05f170bb1d33f0715c7
David Votrubec <david.votrubec@gmail.com>
2016-03-01 10:10:10 +0100

    Added draft of 'what i write' - section for most recent blogposts. So far it is hidden

--- src/app.ts
+++ src/app.ts
@@ -28,11 +28,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
             contact = document.getElementById('contact'),
             work = document.getElementById('work'),
             books = document.getElementById('books'),
-            blogposts = document.getElementById('blogposts'),
+            //blogposts = document.getElementById('blogposts'),
             footer = document.getElementById('footer'),
             skillsetWrapper = document.getElementsByClassName('skills-wrapper')[0];
                 
-        [skillSet, shortBio, contact, work, books, blogposts, footer].forEach((domElement) => {
+        [skillSet, shortBio, contact, work, books, /*blogposts*/, footer].forEach((domElement) => {
            removeClass(domElement, 'visuallyhidden'); 
         });
         
@@ -41,6 +41,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             
             animateProgressBars(skillSet);
         }
+              
+        getBlogPosts();
         
     });
     
--- src/blogposts.ts
+++ src/blogposts.ts
@@ -1,28 +1,35 @@
+// Because I am only rendering few articles here, I do not need fully-blown framework like Angular or React
+// Simple template engine will do
+// First I load articles from Wordpress via WP JSON API
+// then when they are loaded I render them via the TemplateEngine
+// That's all there is to it.
+
+
 // function makeRequest copied from http://stackoverflow.com/a/30008115/552194
 
-function makeRequest (method: string, url: string, otherDomain?: string) {
-  return new Promise(function (resolve, reject) {
+function makeRequest (method: string, url: string) {
+  return new Promise(function (onSuccessFn, onErrorFn) {
     const xhr = new XMLHttpRequest();
     
-    if (otherDomain) {
-        xhr.setRequestHeader('Access-Control-Allow-Origin', otherDomain);
-    }
-    
-    xhr.open(method, url);    
+    xhr.open(method, url);
     
     xhr.onload = function () {
       if (this.status >= 200 && this.status < 300) {
-        resolve(xhr.response);
+          
+          console.log('onload resolve', onSuccessFn, 'bbbbbb');
+          
+          // This throws exception ...  have no idea why
+        //onSuccessFn(xhr.response);
       } else {
-        reject({
+        onErrorFn({
           status: this.status,
           statusText: xhr.statusText
         });
       }
     };
-    
-    xhr.onerror = function () {
-      reject({
+        
+    xhr.onerror = function () {        
+      onErrorFn({
         status: this.status,
         statusText: xhr.statusText
       });
@@ -30,7 +37,38 @@ function makeRequest (method: string, url: string, otherDomain?: string) {
     
     xhr.send();
   });
-}
+};
+
+// TemplateEngine copied from http://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line
+// slightly modified by David Votrubec because of Typescript
+var TemplateEngine = function(html: string, options: any) {
+    var re = /<%([^%>]+)?%>/g, 
+        reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, 
+        code = 'var r=[];\n', 
+        cursor = 0, 
+        match: RegExpMatchArray;
+        
+    var add = function(line, js?) {
+        js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+        return add;
+    }
+    
+    while(match = re.exec(html)) {            
+        var slice = html.slice(cursor, match.index);
+                
+        add(slice)(match[1], true);
+        cursor = match.index + match[0].length;
+    }
+    
+    add(html.substr(cursor, html.length - cursor));
+    
+    code += 'return r.join("");';
+    return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+};
+
+
+const blogPostTemplate = '<div class="post"><%title%></div>';
 
 // Get blogposts via WP Json API
 // The server needs to have CORS enabled, see modified file json-api/json-api.php 
@@ -38,10 +76,24 @@ function makeRequest (method: string, url: string, otherDomain?: string) {
 const url = 'http://blog.davidjs.com?json=1';
 const domain = 'http://blog.davidjs.com';
 
-makeRequest('GET', url, domain)
-.then(function (posts) {
-  console.log(posts);
-})
-.catch(function (err) {
-  console.error('Augh, there was an error!', err.statusText);
-}); 
+function getBlogPosts() {
+    
+    makeRequest('GET', url)
+    .then(function (data: any) {
+        const blogposts = document.getElementById('blogposts');
+        
+        console.log('blogposts', blogposts);
+        
+        debugger
+        const rendered = TemplateEngine(blogPostTemplate, {title: 'test title'});
+        
+        console.log('rendered', rendered);
+        
+        //blogposts.innerHTML = data.posts;
+        blogposts.innerHTML = rendered;
+    })
+    .catch(function (err) {
+        console.error('Augh, there was an error!', err.statusText);
+    });     
+}
+
